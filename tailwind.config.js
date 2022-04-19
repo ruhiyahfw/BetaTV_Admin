fb555f6b06f1d38091276a5d346ce70fdb73dbe7
dwinharits <13519041@std.stei.itb.ac.id>
2022-04-19 12:59:24 +0700

    init admin page versi 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        buletinBlue: "#3F72AF",
+        buletinDarkBlue: "#112D4E",
+        buletinDarkerBlue: "#334257",
+        buletinLightGray: "#DBE2EF",
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
